9bda00351e1ffa5f9b39132e3b58de2765ddf010
Shubham-web289 <77954813+Shubham-web289@users.noreply.github.com>
2025-05-20 00:26:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ def launch_dash_column_editor(df):
         visible_cols = [col["id"] for col in columns]
         return f"✅ Current shape: {df_new.shape[0]} rows × {len(visible_cols)} columns"
 
-    app.run()
+    app.run_server()
 
 
 if __name__ == "__main__":
